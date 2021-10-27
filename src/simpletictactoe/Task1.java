78c7313356169366a52e8baec0af4037b680ccb3
Iv1nko <ivanko170707@gmail.com>
2021-10-27 20:32:59 +0500

    Game2;Task1

--- src/simpletictactoe/Task1.java
+++ src/simpletictactoe/Task1.java
@@ -1,15 +1,12 @@
 package simpletictactoe;
 
-import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String vvod = scanner.next();
-        System.out.println(vvod);
+        String setka = "xoxxoxxox";
 
         for(int i = 0; i < 3; i += 1) {
             int startIndex = i * 3;
-            String res = vvod.charAt(startIndex) + " " + vvod.charAt(startIndex + 1) + " " + vvod.charAt(startIndex + 2);
+            String res = setka.charAt(startIndex) + " " + setka.charAt(startIndex + 1) + " " + setka.charAt(startIndex + 2);
             System.out.println(res);
         }
     }
